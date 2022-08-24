800591c4968974e5a3c592f0f9a0ecc46d69e884
zainabQ1 <111335889+zainabQ1@users.noreply.github.com>
2022-08-24 12:40:50 +0500

    Add files via upload

--- test_math.py
+++ test_math.py
@@ -3,9 +3,27 @@ This module contains basic unit test for math operations
 Their purpose is to show how to use the pytest framework by example
 """
 
+""" Imports """
+import pytest
+
+""" A test case that verifes an exception """
+
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError) as e:
+        num = 1 / 0
+
+        assert 'division by zero' in str(e.value)
+
+
+""" A test case which results in PASS """
+
 
 def test_one_plus_one():
     assert 1 + 1 == 2
+
+
+""" A test case which results in FAIL """
 
 
 def test_one_plus_two():
@@ -13,3 +31,27 @@ def test_one_plus_two():
     b = 2
     c = 0
     assert a + b == c
+
+
+""" A test case which results in Exception """
+
+
+def test_divide_by_zero():
+    num = 1 / 0
+
+
+# Parmetrized
+
+product = [
+    (2, 3, 6),
+    (1, 99, 99),
+    (0, 99, 0),
+    (3, -4, -12),
+    (-5, -5, 25),
+    (2.5, 6.7, 16.75)
+]
+
+
+@pytest.mark.parametrize('a,b,product',product)
+def test_multilication(a, b, product):
+    assert a * b == product
